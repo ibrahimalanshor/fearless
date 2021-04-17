8aa332d03760f2f2fd623faf4c46a67d7974581c
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-17 13:34:24 +0700

    Add Database

--- server/src/App.js
+++ server/src/App.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
 class App {
@@ -8,12 +9,19 @@ class App {
 		this.app = express()
 
 		this.initConfig()
+		this.initDB()
 		this.initRoute()
 	}
 
 	initConfig() {
 		this.app.use(express.urlencoded({ extended: true }))
 		this.app.use(express.json())
+	}
+
+	initDB() {
+		mongoose.connect('mongodb://localhost:27017/fearless', { useNewUrlParser: true, useUnifiedTopology: true })
+			.then(() => console.log('Database Connected'))
+			.catch(err => console.log(err))
 	}
 
 	initRoute() {
