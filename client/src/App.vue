488f358e1dee7f4c3c6c09f99f287ee4c5feb7a9
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-17 10:35:59 +0700

    Add Loading Bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <navbar />
     <router-view></router-view>
+    <vue-progress-bar></vue-progress-bar>
   </div>
 </template>
 
@@ -11,6 +12,22 @@
   export default {
     components: {
       Navbar
+    },
+    mounted() {
+      this.$Progress.finish()
+    },
+    created() {
+      this.$Progress.start()
+
+      this.$router.beforeEach((to, from, next) => {
+        this.$Progress.start()
+
+        next()
+      })
+
+      this.$router.afterEach(() => {
+        this.$Progress.finish()
+      })
     }
   }
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
+import VueProgressBar from 'vue-progressbar'
+
 import App from './App.vue'
 
 import router from './routes'
 import store from './store'
 
 import './assets/css/app.css'
+
+const options = {
+	color: '#3B82F6',
+	failedColor: '#EF4444',
+	thickness: '5px',
+	transition: {
+		speed: '0.2s',
+		opacity: '0.6s',
+		termination: 300
+	},
+	autoRevert: true,
+	inverse: false
+}
+
+Vue.use(VueProgressBar, options)
 
 Vue.config.productionTip = false
 
