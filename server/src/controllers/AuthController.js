792ea1b84ae52084fd928d94defbae5e4ff41eb0
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-24 16:33:03 +0700

    Add Register

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -1,11 +1,19 @@
+const { User } = require('../models')
+
 class AuthController {
 
 	login(req, res) {
 		return res.status(200).json('Login')
 	}
 
-	register(req, res) {
-		return res.status(200).json('Register')
+	async register(req, res) {
+		try {
+			const user = await User.create(req.body)
+
+			return res.status(200).json(req.body)
+		} catch (err) {
+			return res.status(400).json(err)
+		}
 	}
 
 }
