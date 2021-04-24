da37270f7a82b0ff230d6b67bea7d0e1bef81d3d
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-24 23:02:59 +0700

    Fix Register Request Bug

--- server/src/requests/auth/register.js
+++ server/src/requests/auth/register.js
@@ -2,8 +2,8 @@ const { body, validationResult } = require('express-validator')
 const { uniqueUsername, uniqueEmail, confirmed } = require('./validator');
 
 const rules = [
-	body('name').exists().isString(),
-	body('username').exists().isString().custom(uniqueUsername),
+	body('name').exists().isString().isLength({ min: 3 }),
+	body('username').exists().isString().isLength({ min: 3 }).custom(uniqueUsername),
 	body('email').exists().isEmail().custom(uniqueEmail),
 	body('password').exists().isLength({ min: 8 }).custom(confirmed),
 ]
