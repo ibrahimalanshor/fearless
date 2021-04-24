0e5db13946c88c515c992bb842775fa3e7eefaa0
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-24 16:33:54 +0700

    Add Encrypt Password

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
 	name: String,
@@ -6,6 +7,16 @@ const UserSchema = new Schema({
 	email: String,
 	password: String
 })
+
+UserSchema.pre('validate', async function () {
+	await this.encrypt()
+})
+
+UserSchema.methods.encrypt = async function () {
+	const hash = await bcrypt.hash(this.password, 10)
+
+	this.password = hash
+}
 
 const User = model('user', UserSchema)
 
