e4823d6f2b3f293040aa2f59c14169ce5c9d55e7
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-25 23:08:47 +0700

    Add Home Post Component

---
+++ client/src/components/home/HomePost.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="card post">
+		<div class="card-body">
+			<div class="user">
+				<img :src="post.user.photo" class="user-img">
+				<div>
+					<h2 class="user-name">{{ post.user.name }}</h2>
+					<time>{{ post.date }}</time>
+				</div>
+			</div>
+			<div class="content">
+				<p>{{ post.content }}</p>
+				<div v-if="post.images.length">
+					<img v-for="(img, key) in post.images" :key="key" :src="img">
+				</div>
+			</div>
+			<div class="social">
+				<button><i class="far fa-heart social-icon"></i> 22</button>
+				<button><i class="far fa-comment social-icon"></i> 24</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['post']
+	}
+</script>
--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,3 +1,4 @@
 export { default as HomeSidebar } from './HomeSidebar'
 export { default as HomeWidget } from './HomeWidget'
 export { default as HomeCreatePost } from './HomeCreatePost'
+export { default as HomePost } from './HomePost'
