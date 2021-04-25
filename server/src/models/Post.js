ed801e1ab96b545acb737ae0b80dc8b122c8ea9b
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-25 21:01:50 +0700

    Add Getters for Images

--- server/src/models/Post.js
+++ server/src/models/Post.js
@@ -1,13 +1,24 @@
 const { Schema, model } = require('mongoose')
+const { asset } = require('../helpers')
 
 const PostSchema = new Schema({
 	content: String,
-	image: [],
+	images: {
+		type: [String],
+		get: mapImage
+	},
 	user: {
 		type: Schema.Types.ObjectId,
 		ref: 'user'
 	}
 })
+
+function mapImage(images) {
+	return images.map(image => asset(image, 'post_img'))
+}
+
+PostSchema.set('toObject', { getters: true })
+PostSchema.set('toJSON', { getters: true })
 
 const Post = model('post', PostSchema)
 
