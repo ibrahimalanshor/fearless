0c80df39030d736587f0cf2ff4f54dc84c4ebc81
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-25 12:01:08 +0700

    Add Dropdown Component

---
+++ client/src/components/Dropdown.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="relative" v-on-clickaway="close">
+		<div class="h-full cursor-pointer" @click="open = !open">
+			<slot name="toggle"></slot>
+		</div>
+		<transition name="fade">
+			<div v-show="open">
+				<slot></slot>
+			</div>
+		</transition>
+	</div>
+</template>
+
+<script>
+	import { mixin as clickaway } from 'vue-clickaway'
+
+	export default {
+		data() {
+			return {
+				open: false
+			}
+		},
+		mixins: [clickaway],
+		methods: {
+			close() {
+				this.open = false
+			}
+		}
+	}
+</script>
