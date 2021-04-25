b18cbe647adb92b635807523f4be7c135aa39e1d
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-25 12:03:36 +0700

    Add Red Variant For Button

--- client/src/components/auth/LogoutModal.vue
+++ client/src/components/auth/LogoutModal.vue
@@ -13,7 +13,7 @@
 					<p>Are you sure to logout ?</p>
 				</div>
 				<div class="card-foot">
-					<button @click="processLogout" class="button bg-red-600 text-white mr-2">Logout</button>
+					<button @click="processLogout" class="button mr-2">Logout</button>
 					<button class="button gray" @click="close">Cancel</button>
 				</div>
 			</div>
