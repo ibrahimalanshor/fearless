2d80191ca6e8a9fda67398298246758d98ed498d
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-25 12:02:30 +0700

    Add Decode And User Getter

--- client/src/store/auth/getters.js
+++ client/src/store/auth/getters.js
@@ -1,1 +1,12 @@
-export default {}
+import jwt_decode from 'jwt-decode'
+
+export default {
+	decoded: state => {
+		const decoded = jwt_decode(state.user)
+
+		return decoded
+	},
+	user: (state, getters) => {
+		return getters.decoded.user
+	}
+}
