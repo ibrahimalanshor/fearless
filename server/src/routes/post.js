b4d22e3d8b6e28bc4e437bbf72e28467db4a9d69
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-04-25 21:02:03 +0700

    Add Create Route

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -1,7 +1,9 @@
 const { Router } = require('express')
 const router = Router()
 const { PostController } = require('../controllers')
+const { PostRequest } = require('../requests')
 
 router.get('/', PostController.read)
+router.post('/', PostRequest.create, PostController.create)
 
 module.exports = router
