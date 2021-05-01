60f4ffd5fa7d435a79f08a444eaa2c89fcae1ba6
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-05-01 11:45:18 +0700

    Add CORS

--- server/src/App.js
+++ server/src/App.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 
 class App {
@@ -17,6 +18,8 @@ class App {
 	initConfig() {
 		this.app.use(express.urlencoded({ extended: true }))
 		this.app.use(express.json())
+
+		this.app.use(cors())
 
 		this.app.use('/public', express.static(path.join(__dirname + '/../public')))
 	}
